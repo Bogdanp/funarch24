2a21cf40fadb61c87e3fd364d64a397b1a6d32d2
Bogdan Popa <bogdan@defn.io>
2024-05-05 16:06:04 +0300

    update debugging.rkt to include call/comp

--- debugging.rkt
+++ debugging.rkt
@@ -2,6 +2,7 @@
 
 (require (prefix-in dbg: debugging/server))
 (define dbg:stop (dbg:serve))
+(define prompt (make-continuation-prompt-tag))
 
 (define (make-collector)
   (thread
@@ -11,9 +12,20 @@
          (sleep 0.05)
          (loop))))))
 
+(random-seed 1337)
 (with-handlers ([exn:break? void])
-  (let loop ()
-    (make-collector)
-    (sleep 0.1)
-    (loop)))
+  (call-with-continuation-prompt
+   (lambda ()
+     (let loop ()
+       (define k
+         (call-with-composable-continuation
+          (lambda (k)
+            (begin0 k
+              (make-collector)))
+          prompt))
+       (sleep 0.1)
+       (when (< (random) 0.05)
+         (k void))
+       (loop)))
+   prompt))
 (dbg:stop)
