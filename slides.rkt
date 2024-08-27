f7ee8ce52edd838289c31896fb40b097b599535d
Bogdan Popa <bogdan@defn.io>
2024-08-27 15:43:57 +0300

    slides: write Continuations on the Web slide

--- slides.rkt
+++ slides.rkt
@@ -1,6 +1,7 @@
 #lang at-exp slideshow
 
 (require pict
+         slideshow/code
          threading)
 
 (define (author+email name email)
@@ -22,8 +23,72 @@
 (slide
  #:title @~a{Congame vs oTree}) ;; Marc
 
-(slide
- #:title @~a{Continuations on the Web}) ;; Bogdan
+(slide ;; Bogdan
+ #:title @~a{Continuations on the Web}
+ @t{Web programming with continuations simplifies control flow.}
+ 'next
+ 'alts
+ (parameterize ([get-current-code-font-size (λ () 16)])
+   (let ([example-without-continuations
+          (code
+           (define (guess-page req)
+             (define n (random 1 100))
+             (add-cookie
+              "n" (number->string n)
+              (response/xexpr
+               `(form
+                 ([action "/answer"])
+                 (input ([name "guess"]))
+                 (button "Submit")))))
+
+           (define (answer-page req)
+             (define n
+               (let ([s (cookies-ref req "n")])
+                 (and s (string->number s))))
+             (unless n
+               (error "how'd you get here?"))
+             (define guess
+               (string->number (form-ref req "guess")))
+             (response
+              (cond
+                [(= guess n) "You guessed right."]
+                [(< guess n) "Your guess was too low."]
+                [(> guess n) "Your guess was too high."])))
+
+           (register-route! "/answer" answer-page))]
+         [example-with-continuations
+          (code
+           (define (guess-the-number _req)
+             (define n (random 1 100))
+             (define next-req
+               (send/suspend
+                (lambda (k-url)
+                  (response/xexpr
+                   `(form
+                     ([action ,k-url])
+                     (input ([name "guess"]))
+                     (button "Submit"))))))
+             (define guess
+               (string->number (form-ref next-req "guess")))
+             (response/xexpr
+              (cond
+                [(= guess n) "You guessed right."]
+                [(< guess n) "Your guess was too low."]
+                [(> guess n) "Your guess was too high."]))))])
+     (list
+      (list
+       @t{Instead of:}
+       example-without-continuations)
+      (list
+       (ht-append
+        (vc-append
+         10
+         @t{Instead of:}
+         example-without-continuations)
+        (vc-append
+         10
+         @t{We can write:}
+         example-with-continuations)))))))
 
 (slide
  #:title @~a{Mini Congame}) ;; Bogdan
